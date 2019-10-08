a8b65ed77f9edde568479f96aab84ce582476dd9
Vladimir Drachuk <vvdra4@gmail.com>
2019-10-08 11:17:26 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-/**
- * @param preferences - target student focus
- * @param knowsProgramming - if student can do programming and know basics
- * @param config - private student ability to perform for different focus modes
- * @returns number of weeks needed for finish education
- */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
+
+module.exports = function getTimeForEducation(focus, knowsProgramming, config) {
+      for (key in config) {
+        if (key === focus) {
+          if (knowsProgramming) return Math.ceil(800/config[key]);
+          return Math.ceil(1300/config[key]);
+        }
+      }
+
+
   };
   
